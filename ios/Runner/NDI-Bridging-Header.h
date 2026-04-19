1999d8b7bbc54ca709bc02177faf125f373ccd9f
mimo <>
2026-04-19 22:55:37 +0200

    FIX: Add #pragma nullability suppression in Bridging-Header

--- ios/Runner/NDI-Bridging-Header.h
+++ ios/Runner/NDI-Bridging-Header.h
@@ -4,7 +4,10 @@
 // Define that we are linking statically (Required for iOS .a library)
 #define PROCESSINGNDILIB_STATIC 1
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wnullability-completeness"
 // Import the main NDI header (it includes all others: Find, Recv, Send, etc.)
 #import "Processing.NDI.Lib.h"
+#pragma clang diagnostic pop
 
 #endif /* NDI_Bridging_Header_h */
